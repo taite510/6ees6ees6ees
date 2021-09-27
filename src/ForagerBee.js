161f3028c0eefb717f42ccb56a9851a315bed22d
Taite Jernigan <taite5100@gmail.com>
2021-09-27 11:21:07 -0700

    Assignment Complete!

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
-};
+class ForagerBee extends HoneyMakerBee {
+  constructor() {
+    super();
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
